1b3c89bdaf7d8e25552717d8f7c08d1604a983f9
YK <1379327521@qq.com>
2022-08-09 20:28:44 +0800

    api监听

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    "tokenKey":'token',
+    "projectKey":'123456',
+    "reportAddress":'http://hts0000.top:3001'
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import jsError from './lib/jsError'
 import OnLoad from './lib/onLoad'
+import ApiError from './lib/apiError'
+import config from "./config";
 
 
-
-const tokenKey = 'token'
-const projectKey = "1245643154"
+const tokenKey = config.tokenKey
+const projectKey = config.projectKey
 
 const jsErr= new jsError()
 jsErr.jsHandleError()
@@ -13,6 +14,9 @@ jsErr.promiseHandleError()
 const onload = new OnLoad()
 // onload.PV()
 onload.record()
+
+const apiError = new ApiError()
+apiError.apiHandleError()
 
 export {
     tokenKey,
